560a789873179f8bf2c831a05b219fd9908120d8
tomohito-tajima <tomo322017@gmail.com>
2021-11-18 08:45:51 +0000

    Add 会員情報編集画面（管理者側）

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,15 +1,24 @@
 class Admin::CustomersController < ApplicationController
-    
+
   def index
-  end 
-  
+  end
+
   def show
-  end 
-  
+  end
+
   def edit
-  end 
-  
+    @customer = Customer.find(params[:id])
+  end
+
   def update
-  end 
-  
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer.id)
+      flash[:success] = "プロフィールの変更に成功しました"
+    else
+      flash[:danger] = "プロフィールの更新に失敗しました"
+      render :edit
+    end
+  end
+
 end
