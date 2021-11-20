4c86cdc49c83d0b8cfc28c6c4b22a49b76bf6e34
massu0819 <massu4869@gmail.com>
2021-11-20 08:35:15 +0000

    Add トップページ

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,11 +7,9 @@ class Customer < ApplicationRecord
     self.first_name + " " + self.last_name
   end
 
-  enum is_deleted: {有効: false, 退会: true}
-    #有効会員はtrue、退会済み会員はfalse
 
-    def active_for_authentication?
-        super && (self.is_deleted === "Available")
-    end
+    #def active_for_authentication?
+        #super && (self.is_deleted == false)
+   # end
 
 end
