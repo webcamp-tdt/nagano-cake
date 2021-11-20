f54104be7cfbd95511b02ea4b43bdf5b57e2312c
massu0819 <massu4869@gmail.com>
2021-11-20 11:08:48 +0000

    Add 修正

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -10,6 +10,16 @@ class Admin::GenresController < ApplicationController
     redirect_to admin_genres_path
   end 
   
+  def index
+    @genre = Genre.new
+    @genres = Genre.all
+  end
+  
+  def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+  
+  
   private
   
   def genre_params
