d81da8989d4bebd4c6112a91b2ea97d8611ee965
tomohito-tajima <tomo322017@gmail.com>
2021-11-22 07:35:22 +0000

    ADD カートindex

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -1,10 +1,16 @@
 class Customer::CartItemsController < ApplicationController
 
   def index
+    # @cart_items = Cart_item.all
+
+    @cart_items = current_customer.cart_items
   end
 
   def create
-    @cart_item = Cartitem.new(cart_items_params)
+    @cart_item = CartItem.new(cart_items_params)
+    @cart_item.customer_id = current_customer.id
+    @cart_item.save
+    redirect_to cart_items_path
   end
 
   def update
@@ -14,6 +20,11 @@ class Customer::CartItemsController < ApplicationController
   end
 
   def destroy_all
+  end
+
+  private
+  def cart_items_params
+    params.require(:cart_item).permit(:item_id, :quantity)
   end
 
 
--- app/controllers/customer/items_controller.rb
+++ app/controllers/customer/items_controller.rb
@@ -5,6 +5,7 @@ class Customer::ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+    @cart_item = CartItem.new
   end
 
 end
