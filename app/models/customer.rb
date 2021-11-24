f5a442c3b57026745d47d77203b25864892471da
tomohito-tajima <tomo322017@gmail.com>
2021-11-24 12:05:01 +0000

    add 注文詳細

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,5 @@
 class Customer < ApplicationRecord
+  has_many :shippings
 
   has_many :cart_items, dependent: :destroy
   # Include default devise modules. Others available are:
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,5 @@ class Order < ApplicationRecord
   def delivery
       self.post_code + self.address + self.receiver
   end 
+  
 end
